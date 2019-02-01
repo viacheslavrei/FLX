d49581cc4726bef9e7de68d02a637ce0c556b3cb
Viacheslav Boreiko <46229264+viacheslavrei@users.noreply.github.com>
2019-02-02 00:23:31 +0200

    change console.log to alert

--- FLX_homework_6/homework/js/task1.js
+++ FLX_homework_6/homework/js/task1.js
@@ -1,28 +1,20 @@
-/*eslint no-console: ["error", { allow: ["log"] }] */
 var a = (prompt('Enter a')); 
-if (isNaN(parseInt(a))) { 
- console.log("Invalid input data"); 
-}
 var b = (prompt('Enter b'));
-if (isNaN(parseInt(b))) { 
-console.log("Invalid input data");
-}
 var c = (prompt('Enter c'));
-if (isNaN(parseInt(c))) { 
-console.log("Invalid input data");  
-}
-var D = (b*b) - (4 * a * c);
+
+if (isNaN(parseInt(a)) || isNaN(parseInt(b)) || isNaN(parseInt(c))) {
+  alert("Invalid input data");
+} else {
+  var D = (b*b) - (4 * a * c);
   if (D < 0) {
-    console.log("no solution");
-    console.log("Discriminant = " + D);
+    alert("no solution"+"; Discriminant = "+D);
+    
   } else if (D === 0) {
     var S = -b / (2 * a);
-     console.log("x = " + S);
-     console.log("Discriminant = " + D);
+     alert("x = "+S+"; Discriminant = " + D);
   } else {
     var S1 = ( -b + Math.sqrt(D) ) / (2 * a),
       S2 = ( -b - Math.sqrt(D) ) / (2 * a);
-     console.log("x1 = " + S1);
-     console.log("x2 = " + S2);
-     console.log("Discriminant = " + D);
+     alert("x1 = "+S1+"; x2 = "+S2+"; Discriminant = "+D);
   }
+}
