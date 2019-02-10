e921519ed9f39e3c88476ac54be1bad4b8f50be3
Viacheslav Boreiko <46229264+viacheslavrei@users.noreply.github.com>
2019-02-10 23:52:00 +0200

    update task2.js file

--- FLX_homework_7/homework/src/js/task2.js
+++ FLX_homework_7/homework/src/js/task2.js
@@ -1,32 +1,36 @@
 var wantToPlay = confirm('Do you want to play a game?');
 if (wantToPlay) {
-  var maxRange = 5,
-    prize = 0,
-    maxPrize = 10,
-    attemptsCount = 3,
-    currentPrize = maxPrize;
-  while (attemptsCount > 0) {
-    var userChoice = +prompt('Enter a number from 0 to '+maxRange+"\r\n"+
-'Attempts left: '+attemptsCount+"\r\n"+
-'Total prize: '+prize+"\r\n"+
-'Possible prize on current attempt: '+currentPrize)
-    if (userChoice === Math.floor(Math.random() * maxRange)) {
-      prize += currentPrize;
-      var continuePlay = confirm('Congratulation! Your prize is: ' + prize + '. Do you want to continue?');
-      if (continuePlay) {
-        maxPrize *= 3;
+  while (wantToPlay) {
+    var maxRange = 5,
+      prize = 0,
+      maxPrize = 10,
+      attemptsCount = 3,
+      attemptsLeft = 0,
+      tempPrize = 10;
+    var numberInRange = Math.floor(Math.random() * maxRange);
+    while (attemptsLeft < 3) {
+      var userChoice = +prompt('Enter a number from 0 to ' + maxRange + "\r\n" + 
+      'Attempts left: ' + attemptsCount + "\r\n" + 
+      'Total prize: ' + prize + "\r\n" + 
+      'Possible prize on current attempt: ' + tempPrize);
+      if (userChoice === numberInRange) {
+        prize += tempPrize;
         attemptsCount = 3;
+        attemptsLeft = 0;
+        maxPrize *= 3;
         maxRange *= 2;
-        currentPrize = maxPrize;
+        tempPrize = maxPrize;
+        numberInRange = Math.floor(Math.random() * maxRange);
+        wantToPlay = confirm('Congratulation! Your prize is: ' + prize + '. Do you want to continue?');
       } else {
-        alert('Thank you for a game. Your prize is: ' + prize);
+        attemptsLeft++;
+        attemptsCount--;
+        tempPrize = Math.floor(maxPrize / (attemptsLeft * 2));
       }
-    } else {
-      currentPrize = Math.floor(currentPrize / 2);
-      attemptsCount--;
     }
+    alert('Thank you for a game. Your prize is: ' + prize);
+    wantToPlay = confirm('Do you want to try again?');
   }
-  alert('Thank you for a game. Your prize is: ' + prize);
 } else {
   alert('You did not become a millionaire, but can.');
 }
