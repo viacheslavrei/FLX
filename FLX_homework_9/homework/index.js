e3e101e7fb1dd2006a87502a2775a61739ab41c4
Viacheslav Boreiko <46229264+viacheslavrei@users.noreply.github.com>
2019-02-16 18:01:05 +0200

    upload homework_9 files

---
+++ FLX_homework_9/homework/index.js
@@ -0,0 +1,118 @@
+var data = [{
+  "_id": "5b5e3168c6bf40f2c1235cd6",
+  "index": 0,
+  "age": 39,
+  "eyeColor": "green",
+  "name": "Stein",
+  "favoriteFruit": "apple"
+}, {
+  "_id": "5b5e3168e328c0d72e4f27d8",
+  "index": 1,
+  "age": 38,
+  "eyeColor": "blue",
+  "name": "Cortez",
+  "favoriteFruit": "strawberry"
+}, {
+  "_id": "5b5e3168cc79132b631c666a",
+  "index": 2,
+  "age": 2,
+  "eyeColor": "blue",
+  "name": "Suzette",
+  "favoriteFruit": "apple"
+}, {
+  "_id": "5b5e31682093adcc6cd0dde5",
+  "index": 3,
+  "age": 19,
+  "eyeColor": "green",
+  "name": "George",
+  "favoriteFruit": "banana"
+}]
+//1
+function findTypes() {
+  var args = [];
+  for (var i = 0; i < arguments.length; i++) {
+    args[i] = (typeof(arguments[i]));
+  }
+  return args;
+}
+findTypes(null, 5, 'hello')
+//2
+function executeforEach(arr, func) {
+  for (var i = 0; i < arr.length; i++) {
+    func(arr[i]);
+  }
+}
+executeforEach([1, 2, 3], function(el) {
+  console.log(el)
+})
+//3
+function mapArray(arr, func) {
+  var transformed = [];
+  executeforEach(arr, function(el) {
+    transformed.push(func(el))
+  });
+  return transformed;
+}
+mapArray([2, 5, 8], function(el) {
+  return el + 3
+})
+//4
+function filterArray(arr, func) {
+  var transformed = [];
+  executeforEach(arr, function(el) {
+    if (func(el)) {
+      transformed.push(el);
+    }
+  });
+  return transformed;
+}
+filterArray([2, 5, 8], function(el) {
+  return el > 3
+})
+//5
+function getAmountOfAdultPeople(insertData) {
+  return filterArray(insertData, function(el) {
+    el.age > 18
+  }).length;
+}
+getAmountOfAdultPeople(data);
+//6
+function getGreenAdultBananaLovers(insertData) {
+  var filterPeople = filterArray(insertData, function(el) {
+    el.age > 18 && el.favoriteFruit === "banana" && el.eyeColor === "green"
+  });
+  return mapArray(filterPeople, function(el) {
+    el.name
+  });
+}
+getGreenAdultBananaLovers(data);
+//7
+function keys(arr) {
+  var ids = Object.keys(arr);
+  return ids;
+}
+keys({ keyOne: 1, keyTwo: 2, keyThree: 3 })
+//8
+function values(arr) {
+  var ids = Object.values(arr);
+  return ids;
+}
+values({ keyOne: 1, keyTwo: 2, keyThree: 3})
+//9
+function showFormattedDate(date) {
+  var month = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+  return 'Date: ' + date.getDate() + ' of ' + month[date.getMonth()] + ', ' + date.getFullYear();
+}
+showFormattedDate(new Date('2019-01-27T01:10:00'))
+//10
+function isEvenYear(date) {
+  var year = date.getFullYear();
+  return (year) % 2 === 0;
+}
+isEvenYear(new Date('2019-01-27T01:10:00'))
+//11
+function isEvenMonth(date) {
+  var month = date.getMonth();
+  return (month + 1) % 2 === 0;
+}
+isEvenMonth(new Date('2019-02-27T01:10:00'))
